7e1c8d120d93c2ca8dbaae0d5450a7349295329d
getssh <gtamrat33@gmail.com>
2022-11-27 00:04:37 +0300

    mobile resposiveness added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import { useState, React } from 'react';
 import './App.css';
 import Form from './components/Form/Form'
+import Cardfront from './images/bg-card-front.png'
+import Cardback from './images/bg-card-back.png'
 
 function App() {
   const [card, setCard] = useState({
-    name: "",
+    name: "Abebe Kebede",
     cardNumber: '0000 0000 0000 0000',
-    exMonth: "",
-    exYear: "",
+    exMonth: "00",
+    exYear: "00",
     cvc: ""
   })
 
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className='col-xsm-12 col-sm-6 left'>
-          <h1 className='text-white'>hello</h1>
+        <div className='col-sm-12 col-md-6 left'>
+          <div className='row card-front'>
+              <img src={Cardfront} width="100%" />
+              <div className='on-image'>
+                <p>{card.cardNumber}</p>
+              </div>
+          </div>
+          <div className='row card-back'>
+              <img src={Cardback} />
+          </div>
         </div>
-        <div className='col-xsm-12 col-sm-6 right'>
+        <div className='col-sm-12 col-md-6 right'>
           <form className='card-form'>
             <Form
             label="Cardholder Name"
@@ -31,7 +41,7 @@ function App() {
             type="text"
             name="cardNumber"
             placeholder="e.g. 1234 5678 9123 0000"
-            value={card.name}
+            value={card.cardNumber}
             />
             <div className='card-bottom'>
                 <Form
@@ -39,21 +49,21 @@ function App() {
                 type="text"
                 name="exMonth"
                 placeholder="MM"
-                value={card.name}
+                value={card.exMonth}
                 />
                 <Form
                 label="(MM/YY)"
                 type="text"
                 name="exYear"
                 placeholder="YY"
-                value={card.name}
+                value={card.exYear}
                 />
                 <Form
                 label="CVC"
                 type="text"
                 name="cvc"
                 placeholder="e.g. 123"
-                value={card.name}
+                value={card.cvc}
                 />
             </div>
             <button className='submit'>Confirm</button>
